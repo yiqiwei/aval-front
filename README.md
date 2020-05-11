e692f805616e369c0fa31a57124304346f42ed66
yiqiwei <yiqiwei333@gmail.com>
2020-05-11 22:55:31 +1000

    Initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# aval-front
-Created with CodeSandbox
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "antd/dist/antd.css";
+import Login from "./login";
+
+ReactDOM.render(<Login />, document.querySelector("#root"));
